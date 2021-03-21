d8bd4e043dda905295b0b96c7c7bb02b527f62f4
kohmatsu111 <voy.al.cabo@gmail.com>
2021-03-21 16:39:59 +0900

    Dog.ktのエラーコード

---
+++ app/src/main/java/jp/techacademy/masahiro/kohmatsu/kotlinlog/Dog.kt
@@ -0,0 +1,20 @@
+package jp.techacademy.taro.kirameki.kotlinlog
+
+import android.util.Log
+
+class Dog {
+    // プロパティ
+    var name: String
+    var age: Int
+
+    // コンストラクタ
+    constructor() {
+        this.name = ""
+        this.age = 0
+    }
+
+    // メソッド
+    fun say() {
+        Log.d("kotlintest", this.name + "(" + this.age + "歳)" + "「ワンワン」")
+    }
+}
